ab852c846a1bca10c4cc8d5ab158031307557183
zuhoor2590 <163122132+zuhoor2590@users.noreply.github.com>
2025-04-04 23:36:26 -0700

    Update mysqlconnection.py

--- soloApp/flask_app/config/mysqlconnection.py
+++ soloApp/flask_app/config/mysqlconnection.py
@@ -1,45 +1,46 @@
-# a cursor is the object we use to interact with the database
+# connection.py
 import pymysql.cursors
-# this class will give us an instance of a connection to our database
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from a .env file (recommended)
+load_dotenv()
+
 class MySQLConnection:
     def __init__(self, db):
-        # change the user and password as needed
-        connection = pymysql.connect(host = 'database-1.cedo8qm2k05m.us-east-1.rds.amazonaws.com',
-                                    user = 'root', 
-                                    password = 'Kabul_2525', 
-                                    db = 'socialdb',
-                                    charset = 'utf8mb4',
-                                    cursorclass = pymysql.cursors.DictCursor,
-                                    autocommit = False)
-        # establish the connection to the database
+        # Load credentials securely
+        connection = pymysql.connect(
+            host=os.getenv('DB_HOST', 'database-1.cedo8qm2k05m.us-east-1.rds.amazonaws.com'),
+            user=os.getenv('DB_USER', 'root'),
+            password=os.getenv('DB_PASSWORD', 'Kabul_2525'),
+            db=db,
+            charset='utf8mb4',
+            cursorclass=pymysql.cursors.DictCursor,
+            autocommit=False
+        )
         self.connection = connection
-    # the method to query the database
-    def query_db(self, query:str, data:dict=None):
+
+    def query_db(self, query: str, data: dict = None):
         with self.connection.cursor() as cursor:
             try:
                 query = cursor.mogrify(query, data)
                 print("Running Query:", query)
-    
+
                 cursor.execute(query)
                 if query.lower().find("insert") >= 0:
-                    # INSERT queries will return the ID NUMBER of the row inserted
                     self.connection.commit()
                     return cursor.lastrowid
                 elif query.lower().find("select") >= 0:
-                    # SELECT queries will return the data from the database as a LIST OF DICTIONARIES
                     result = cursor.fetchall()
                     return result
                 else:
-                    # UPDATE and DELETE queries will return nothing
                     self.connection.commit()
             except Exception as e:
-                # if the query fails the method will return FALSE
-                print("Something went wrong", e)
+                print("Something went wrong:", e)
                 return False
             finally:
-                # close the connection
-                self.connection.close() 
-# connect_to_mysql receives the database we're using and uses it to create an instance of MySQLConnection
+                self.connection.close()
+
 def connectToMySQL(db):
     return MySQLConnection(db)
 
